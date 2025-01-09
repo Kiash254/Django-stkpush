ca89a6cf1fe8818e3a7ae0df4c668d306c62f4b1
Kiash254 <codewithkiash@gmail.com>
2025-01-09 13:27:54 +0300

    corrected the rror on the page

--- config/settings.py
+++ config/settings.py
@@ -102,12 +102,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'frontend/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static')
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'stkpush:login'
