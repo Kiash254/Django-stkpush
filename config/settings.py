2c4599e8b0dac6b621abd7dcd1b4bbe855af9520
Kiash254 <codewithkiash@gmail.com>
2025-01-09 13:19:05 +0300

    added new method for debugs

--- config/settings.py
+++ config/settings.py
@@ -2,12 +2,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+import os
 SECRET_KEY = 'django-insecure-t$-%+s$8n27@1i(ht00s9kpiqy6&6h)7)$3f--i2h)=k*pzmu)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -45,7 +40,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates' ,
+           os.path.join(BASE_DIR, 'frontend/templates') 
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -107,9 +102,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'frontend/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'frontend/static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
